bcf1d202ecca479b4b47201f2b10a1a90706a4d4
thaonk <thaonk@vng.com.vn>
2019-11-15 20:41:00 +0700

    1

--- app/src/components/Admin/Customer.js
+++ app/src/components/Admin/Customer.js
@@ -31,12 +31,7 @@ const Customer = (props) => {
     // };
     const handleButtonSearch = () => {
         var arr = []
-        arr = listCustomerTemp.filter(e => e.ten.toLowerCase().includes(searchValue.toLowerCase()));
-        arr = arr.concat(listCustomerTemp.filter(e => e.biensoxe.toLowerCase().includes(searchValue.toLowerCase())));
-        arr.filter((e,index) => {
-            if(arr.indexOf(e) === index)
-                return e;
-        })
+        arr = listCustomerTemp.filter(e => e.ten.toLowerCase().includes(searchValue.toLowerCase()) || e.biensoxe.toLowerCase().includes(searchValue.toLowerCase() ));
         setlistCustomer(arr);
     };
     const _handleKeyPress = (e) => {

f9dcfb4b49c79c26949cd0c7a075baf6b5c9025d
thaonk <thaonk@vng.com.vn>
2020-04-20 16:53:41 +0700

    update port

--- app/src/Config/index.js
+++ app/src/Config/index.js
@@ -1,4 +1,4 @@
 // export const HOST = "https://qlnx.herokuapp.com";
 // export const HOST = "http://18.140.53.84:8080";
 // export const HOST = "http://192.168.1.2";
-export const HOST = "http://192.168.1.160:8080";
+export const HOST = "http://trungtrang.myddns.me:8080";

39c2e35abf753e947c9185f3dba92a5a310ac71e
thaonk <taonuaa004@gmail.com>
2020-09-08 10:19:01 +0700

    dsad

--- app/src/Config/index.js
+++ app/src/Config/index.js
@@ -3,5 +3,5 @@
 // export const HOST = "http://192.168.1.2";
 // export const HOST = "http://127.0.0.1:8080";
 // export const HOST = "http://trungtrang.myddns.me:8080";
-export const HOST = "http://192.168.1.230:8080";
+export const HOST = "http://115.75.89.137:8080";
 export const HOST_SHEME = "trungtrang://trungtrang.myddns.me";

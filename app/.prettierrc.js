3c6291f1d838e13972d1fa5ee29975aca70bba3c
thaonk <taonuaa004@gmail.com>
2022-10-09 00:23:44 +0700

    add pretirerr

---
+++ app/.prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    trailingComma: "es5",
+    tabWidth: 4,
+    semi: true,
+    printWidth: 150,
+    bracketSpacing: true,
+    bracketSameLine: false,
+    jsxBracketSameLine: false,
+    arrowParens: "always",
+    singleQuote: false,
+    endOfLine: "crlf",
+};

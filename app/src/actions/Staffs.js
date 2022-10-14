6c3bcb04b389c73762299361aa2ad9ed9611951b
thaonk <taonuaa004@gmail.com>
2022-10-14 23:15:34 +0700

    add action

--- app/src/actions/Staffs.js
+++ app/src/actions/Staffs.js
@@ -1,26 +1,59 @@
-import { GetListStaff } from "../API/Staffs";
+import * as type from "./action-types";
+import APIUtils from "../API/APIUtils";
 
-export const REQUEST_LIST_STAFF = "REQUEST_LIST_STAFF";
-export const REQUEST_LIST_STAFF_SUCCESS = "REQUEST_LIST_STAFF_SUCCESS";
-export const REQUEST_LIST_STAFF_FAILER = "REQUEST_LIST_STAFF_FAILER";
-
-export const getListStaff = (token) => (dispatch) => {
+export const getListStaff = () => (dispatch) => {
     dispatch({
-        type: REQUEST_LIST_STAFF,
+        type: type.STAFFS.LOADING_STAFFS,
+        data: true,
     });
-    return GetListStaff(token)
-        .then((response) => {
-            console.log(response.data);
+
+    return APIUtils.get("/employee")
+        .then((res) => {
             dispatch({
-                type: REQUEST_LIST_STAFF_SUCCESS,
-                data: response.data,
+                type: type.STAFFS.GET_LIST_STAFFS,
+                data: res,
             });
         })
         .catch((error) => {
-            if (error)
-                // dispatch(showNoti('Danger', error.response.data.message))
-                dispatch({
-                    type: REQUEST_LIST_STAFF_FAILER,
-                });
+            dispatch({
+                type: "SWAP_ALERT",
+                data: {
+                    isLoading: true,
+                    error: 1,
+                    message: error.message,
+                },
+            });
+            dispatch({
+                type: type.STAFFS.LOADING_STAFFS,
+                data: true,
+            });
         });
+};
+
+export const addStaff = (data) => (dispatch) => {
+    return APIUtils.post("/employee", data).then((res) => {
+        dispatch({
+            type: type.STAFFS.ADD_STAFFS,
+            data: data,
+        });
+    });
+};
+
+export const updateStaff = (data, ma) => (dispatch) => {
+    return APIUtils.put("/employee/ma/" + ma, data).then((res) => {
+        dispatch({
+            type: type.STAFFS.UPDATE_STAFFS,
+            data: data,
+            ma: ma,
+        });
+    });
+};
+
+export const deleteStaff = (ma) => (dispatch) => {
+    return APIUtils.delete("/employee/ma/" + ma).then((res) => {
+        dispatch({
+            type: type.STAFFS.DELETE_STAFFS,
+            ma: ma,
+        });
+    });
 };

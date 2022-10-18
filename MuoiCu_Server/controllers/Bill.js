a7144683da3fffe7adaa1156d8360a29cff3e02c
thaonk <taonuaa004@gmail.com>
2022-10-18 20:19:13 +0700

    fix bug

--- MuoiCu_Server/controllers/Bill.js
+++ MuoiCu_Server/controllers/Bill.js
@@ -57,7 +57,7 @@ module.exports = {
                 param['ngaysuachua'] = param["ngaythanhtoan"];
                 param["trangthai"] = 1;
                 let resulft = await Abstract.update(Bill, param, req.params);
-                await BillSuachua.giamSoLuongPhuTung(req.params.mahoadon);
+                
                 setTimeout(async function () {
                     try {
                         var kh = await Abstract.getOne(Customer, { ma: hoadon.makh });

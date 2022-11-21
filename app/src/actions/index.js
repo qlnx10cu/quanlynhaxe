0baca4f1f4cf8cff6516b81a75bb6ad2f6e64714
thaonk <taonuaa004@gmail.com>
2022-11-21 20:21:27 +0700

    rename product

--- app/src/actions/index.js
+++ app/src/actions/index.js
@@ -2,6 +2,6 @@ export * as StaffAction from "./StaffAction";
 export * as SalaryAction from "./SalaryAction";
 export * as CustomerAction from "./CustomerAction";
 export * as StoreOutsideAction from "./StoreOutsideAction";
-export * as ProductAction from "./Product";
+export * as ProductAction from "./ProductAction";
 export * as RepairedAction from "./RepairedAction";
 export * as RetailAction from "./RetailAction";

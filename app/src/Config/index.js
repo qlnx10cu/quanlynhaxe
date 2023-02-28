50c853c943825619820e3b4f30c414bf8965d67f
thaonk <taonuaa004@gmail.com>
2023-02-28 13:08:52 +0700

    add ui

--- app/src/Config/index.js
+++ app/src/Config/index.js
@@ -1,4 +1,4 @@
-export const TYPE_SERVER = window.location.hostname.includes("bk.trungtrang.com") ? 1 : 0;
+export const TYPE_SERVER = window.location.hostname.includes("mx.trungtrang.com") ? 1 : 0;
 const port = TYPE_SERVER == 1 ? "8081" : "8080";
 const url = window.location.protocol + "//" + window.location.hostname + ":" + port;
 export const HOST = window.host_url || (process.env.NODE_ENV == "production" ? url : "http://127.0.0.1:" + port);

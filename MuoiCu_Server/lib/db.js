5025173a7576d6888f818f3293b8c6ea73f2a51c
nhthan <nhthan@gmail.com>
2025-05-05 15:30:33 +0700

    fix: pool

--- MuoiCu_Server/lib/db.js
+++ MuoiCu_Server/lib/db.js
@@ -1,51 +1,18 @@
 const pool = require('./mysql');
 const logger = require("../lib/logger");
 
-function query(query, param) {
-    logger.warn({ query: query, param: param });
+function query(sql, params) {
+    logger.warn({ query: sql, param: params });
+
     return new Promise((resolve, reject) => {
-        pool.getConnection((err, connection) => {
-            if (err) return reject(err);
-            connection.setMaxListeners(50);
-            connection.on('error', (error) => {
-                try {
-                    connection.release();
-                } catch (ex) {
-
-                }
-                console.log(error);
-                logger.error({ error: error, query: query, param: param });
+        pool.query(sql, params, (error, results) => {
+            if (error) {
+                logger.error({ error, query: sql, param: params });
                 return reject(error);
-            })
-
-            if (param) {
-                connection.on('query', function (query) {
-                    console.log(query.sql);
-                });
-                connection.query(query, param, (error, rows) => {
-                    try {
-                        connection.release();
-                    } catch (ex) {
-
-                    }
-                    if (error) return reject(error);
-                    return resolve(rows);
-                })
-            } else {
-                connection.query(query, (error, rows) => {
-                    try {
-                        connection.release();
-                    } catch (ex) {
-
-                    }
-                    console.log(error);
-                    if (error) return reject(error);
-                    return resolve(rows);
-                })
             }
-
-        })
-    })
+            resolve(results);
+        });
+    });
 }
 
 module.exports = query;
